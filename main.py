b538c9f394c535aa9706c84a7861a1d54ff13f17
Max Clinton <maxtclinton@gmail.com>
2023-04-04 22:10:39 -0400

    Add humidity

--- main.py
+++ main.py
@@ -20,5 +20,6 @@ def main():
 
     weather = requests.get(url).json()
     print(f"{str(weather['current_weather']['temperature'])} {temperature_unit}")
+    print(weather['hourly']['relativehumidity_2m'][int(weather['current_weather']['time'][11:13])])
 if __name__ == "__main__":
     main()
